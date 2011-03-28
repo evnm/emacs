2401907822c809ff8caab275e423c6b9d5c6b291
Evan Meagher <evan.meagher@gmail.com>
2011-03-28 00:17:20 -0700

    Rm default frame size setting.

--- init.el
+++ init.el
@@ -40,5 +40,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default-frame-alist (quote ((height . 47) (width . 85) (tool-bar-lines . 0) (menu-bar-lines . 0))))
  '(gud-gdb-command-name "gdb --annotate=1"))

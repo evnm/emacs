03e4a44e49955e089c80d0c528183d48937ccba3
Evan Meagher <evan.meagher@gmail.com>
2011-03-31 22:23:20 -0700

    Rm last vestige of custom-set-variables.

--- init.el
+++ init.el
@@ -38,10 +38,3 @@
                                   (symbol-name system-type) ".el")))
   (if (file-exists-p system-type-config)
       (load system-type-config)))
-
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(gud-gdb-command-name "gdb --annotate=1"))

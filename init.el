67d93d367ab060fa8facaecb8e282dbe236e59ef
Evan Meagher <evm@twitter.com>
2014-12-29 15:12:23 -0800

    Add `package-initialize` to init.el

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
 
 ; Load personal customizations
 (mapc 'load-directory '("~/.emacs.d/customizations"))
